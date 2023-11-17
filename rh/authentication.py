25aa06c8c319df2ce9d41608b95b29b6e2557c2c
Camilo Cota <ccota@redhat.com>
2023-11-17 20:36:08 +0100

    POC: add view group configuration permission to the user

--- rh/authentication.py
+++ rh/authentication.py
@@ -3,6 +3,9 @@ from crum import impersonate
 from djangosaml2.backends import Saml2Backend
 from dojo.models import Dojo_Group, Dojo_Group_Member, Role, Dojo_User
 from dojo.authorization.roles_permissions import Roles
+from django.contrib.auth.models import Permission
+from dojo.authorization.authorization import user_has_configuration_permission
+from dojo.authorization.roles_permissions import Permissions
 
 logger = logging.getLogger("saml2")
 
@@ -10,11 +13,20 @@ class ModifiedSaml2Backend(Saml2Backend):
     def _update_user(self, user, attributes: dict, attribute_mapping: dict, force_save: bool = False):
         if 'groups' in attributes:
             group_names = attributes['groups']
+
+            # Users have to be authorized to view groups in general to view their own groups
+            # https://github.com/DefectDojo/django-DefectDojo/blob/master/dojo/group/views.py#L95-L97
+            # TESTING PURPOSES: Don't use in production the group can contain user information that might
+            # be considered confidential
+            if not user_has_configuration_permission(user, 'view_group'):
+                user.user_permissions.add(Permission.objects.get(codename='view_group'))
+                logger.debug("User %s has been granted group view permission", user)
+
             for group_name in group_names:
                 created_group = False
 
                 # By default, when a group is created, the owner is automatically assigned as the currently logged-in user.
-                # However, in this particular scenario, our requirement is to designate the admin as the owner.
+                # However, in this particular scenario, assigning the admin as the owner seems sensible
                 with impersonate(Dojo_User.objects.get(username='admin')):
                     group, created_group = Dojo_Group.objects.get_or_create(name=group_name)
 
@@ -23,12 +35,8 @@ class ModifiedSaml2Backend(Saml2Backend):
 
                 # We must save the user; otherwise, Django will not create the membership
                 # see: https://docs.djangoproject.com/en/1.10/releases/1.8/#assigning-unsaved-objects-to-relations-raises-an-error
-                user.save()
+                user = self.save_user(user)
 
-                # A potential bug has been identified in DefectDojo:
-                # when a user lacks the "Group Viewer" permission in Configuration Permissions, they are unable to view the details of
-                # their assigned groups, regardless of their role within those groups.
-                # https://defectdojo.github.io/django-DefectDojo/usage/permissions/#groups
                 group_member, is_member_created = Dojo_Group_Member.objects.get_or_create(group=group, user=user,
                     defaults={'role': Role.objects.get(id=Roles.Maintainer)})
 
